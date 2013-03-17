5d6748de34ac09a22bbe91862b19ad755ab33ec5
Andrea Turso <trashofmasters@gmail.com>
2013-03-17 17:21:41 +0000

    PHPUnit and Behat have been set as core dependencies.

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -13,12 +13,18 @@ if (! function_exists('includeIfExists')) {
         if (file_exists($file)) {
             return include $file;
         }
+        return false;
     }
 }
 
-attempt:
-if ((!$loader = includeIfExists(__DIR__ . '/../vendor/autoload.php'))
-    && (!$loader = includeIfExists(__DIR__ . '/../../../autoload.php'))) {
+if (! function_exists('tryIncludeAutoloader')) {
+    function tryIncludeAutoloader() {
+        return includeIfExists(__DIR__ . '/../vendor/autoload.php')
+            || includeIfExists(__DIR__ . '/../../../autoload.php');
+    }
+}
+
+if (! $loader = tryIncludeAutoloader()) {
     file_put_contents(
         sys_get_temp_dir() . '/installer.php',
         file_get_contents('https://getcomposer.org/installer')
@@ -26,8 +32,7 @@ if ((!$loader = includeIfExists(__DIR__ . '/../vendor/autoload.php'))
     system('php ' . sys_get_temp_dir() . '/installer.php');
     system('php composer.phar install');
 
-    goto attempt;
+    $loader = tryIncludeAutoloader();
 }
-
 
 return $loader;
