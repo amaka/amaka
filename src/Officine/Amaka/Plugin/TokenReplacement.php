be5aca62b9ad5078f41be368ce8cbb9af390ef54
Andrea Turso <trashofmasters@gmail.com>
2013-03-24 14:22:13 +0000

    Added doc-blocks to the TokenReplacement plugin.
    TODO:
    - test replaceFromInto(..., $destination) where $destination exists (as file and as a directory)
    - test the behaviour of replacing unbound tokens: do we i) keep them unchanged or do we ii) replace them with nothing?
    - add FileSet to allow replaceInto to operate on fileset
    - allow using a closure to map elements in the FileSet to their respective $destination name, for use with replaceFromInto
      e.g. ```replaceFromInto($fileset, function($file) { return str_replace('*.dist', '', $file->getFilename()); });```

--- src/Officine/Amaka/Plugin/TokenReplacement.php
+++ src/Officine/Amaka/Plugin/TokenReplacement.php
@@ -4,15 +4,32 @@ namespace Officine\Amaka\Plugin;
 
 use Officine\Amaka\PluginInterface;
 
+/**
+ * Token Replacement plugin
+ */
 class TokenReplacement implements PluginInterface
 {
     private $map = array();
 
+    /**
+     * Interpret the value of $token
+     *
+     * Silently returns null when $token is undefined.
+     *
+     * @return mixed|null
+     */
     public function interpret($token)
     {
         return $this->map[$token];
     }
 
+    /**
+     * Bind a $value to a particular $token
+     *
+     * @param string $token
+     * @param mixed $value
+     * @return $this
+     */
     public function bind($token, $value)
     {
         $this->map[$token] = $value;
@@ -20,6 +37,16 @@ class TokenReplacement implements PluginInterface
         return $this;
     }
 
+    /**
+     * Replace the tokens contained inside $file.
+     *
+     * The file is saved under the same name after processing, if you
+     * need to replace the tokens and save under a different file name
+     * please {@see replaceFromInto($source, $destination)}
+     *
+     * @param $file
+     * @return $this
+     */
     public function replaceInto($file)
     {
         $toks = array_keys($this->map);
@@ -29,12 +56,21 @@ class TokenReplacement implements PluginInterface
         $updated = str_replace($toks, $vals, $content);
 
         file_put_contents($file, $updated);
+
+        return $this;
     }
 
+    /**
+     * Replace the tokens contained in $source inside a the new $destination file.
+     *
+     * @param string $source
+     * @param string $destination
+     */
     public function replaceFromInto($source, $destination)
     {
         if (copy($source, $destination)) {
             $this->replaceInto($destination);
         }
+        return $this;
     }
 }
